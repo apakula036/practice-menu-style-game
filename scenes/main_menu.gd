5dc587578bfc282e775699adecca63362c246946
Andrew <70990567+apakula036@users.noreply.github.com>
2025-12-06 19:40:45 -0800

    added script to make buttons print something

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,1 +1,13 @@
 extends Control
+
+
+func _on_startgamebutton_pressed() -> void:
+	print("start")
+
+
+func _on_optionsbutton_pressed() -> void:
+	print("options")
+
+
+func _on_quitgamebutton_pressed() -> void:
+	print("quit")
