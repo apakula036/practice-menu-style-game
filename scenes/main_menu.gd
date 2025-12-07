ff7a9b396ed459f1c05712b4a752f946658ecc96
Andrew <70990567+apakula036@users.noreply.github.com>
2025-12-06 20:12:42 -0800

    added new scene and edited scripts

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 
 func _on_startgamebutton_pressed() -> void:
 	print("start")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_optionsbutton_pressed() -> void:
@@ -10,4 +11,4 @@ func _on_optionsbutton_pressed() -> void:
 
 
 func _on_quitgamebutton_pressed() -> void:
-	print("quit")
+	get_tree().quit()
