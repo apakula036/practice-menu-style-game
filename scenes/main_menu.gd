c6d839ceecb55defbdd00fbc6c705ffa76ef454e
Andrew <70990567+apakula036@users.noreply.github.com>
2025-12-06 20:42:37 -0800

    Adding git ignore
    Adding git ignore

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -2,13 +2,14 @@ extends Control
 
 
 func _on_startgamebutton_pressed() -> void:
-	print("start")
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	print("start button pressed")
+	get_tree().change_scene_to_file("res://scenes/gamemain.tscn")
 
 
 func _on_optionsbutton_pressed() -> void:
-	print("options")
+	print("options button pressed")
 
 
 func _on_quitgamebutton_pressed() -> void:
+	print("quit button pressed")
 	get_tree().quit()
